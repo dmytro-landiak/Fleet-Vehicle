e1f68d07339b7da09aceb499c8be678680a8469f
Dmytro Landiak <n0fear2110@gmail.com>
2017-05-24 19:33:26 +0300

    init commit

---
+++ src/main/java/com/landim/entity/Driver.java
@@ -0,0 +1,49 @@
+package com.landim.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by n0fea on 24.05.2017.
+ */
+public class Driver implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer driverID;
+    private String driverName;
+    private String phone;
+
+
+    public Driver(String driverName, String phone) {
+        this.driverName = driverName;
+        this.phone = phone;
+    }
+
+    public Driver() {}
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getDriverID() {
+        return driverID;
+    }
+
+    public void setDriverID(Integer driverID) {
+        this.driverID = driverID;
+    }
+
+}
+
+
---
+++ src/main/java/com/landim/entity/User.java
@@ -0,0 +1,59 @@
+package com.landim.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer userID;
+    private String name;
+    private String eMail;
+    private String password;
+
+    public User(String name, String eMail, String password) {
+        this.name = name;
+        this.eMail = eMail;
+        this.password = password;
+    }
+
+    public User() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+}
